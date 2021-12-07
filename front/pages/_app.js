bde0ff41f1828b62e40aa99d70d5af1a4816daf6
ParkSungHyun <psh1030.dev@gmail.com>
2021-12-07 16:47:45 +0900

    feat: add antd design
    antd design is add to make it easier to design
    Meta information was added using the HEAD of 'next/head'
    A search button has been added to the AppLayout

---
+++ front/pages/_app.js
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+import "antd/dist/antd.css";
+import Head from "next/head";
+
+const App = ({ Component }) => {
+  return (
+    <>
+      <Head>
+        <title>NodeBird</title>
+        <meta charset="utf-8" />
+      </Head>
+      <Component />
+    </>
+  );
+};
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+};
+
+export default App;
+
+// Page들의 공통부분
+// index.js 의 Home이 component로 들어와서 랜더링 된다.
