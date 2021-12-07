0e189e572092b06ee0129eab98a354b9b5a1eaed
ParkSungHyun <psh1030.dev@gmail.com>
2021-12-07 15:27:59 +0900

    feat: make folder structure
    To start project, make folder structure
    Component: Reusable module
    Page: the unit page of the spa covered by next.js

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,13 +1,27 @@
 import PropTypes from "prop-types";
+import Link from "next/link";
 
-const AppLayout = ({children}) => {
-  return <div>{children}</div>;
+const AppLayout = ({ children }) => {
+  return (
+    <div>
+      <div>
+        <Link href="/">
+          <a>노드버드</a>
+        </Link>
+        <Link href="/profile">
+          <a>프로필</a>
+        </Link>
+        <Link href="/signup">
+          <a>회원가입</a>
+        </Link>
+      </div>
+      {children}
+    </div>
+  );
 };
 
-export default AppLayout;
-
-AppLayout.propTypes={
-    children:PropTypes.node.isRequired,
-}
+AppLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default AppLayout;
